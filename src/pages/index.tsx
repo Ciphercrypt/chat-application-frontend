e835363469e1d769f020b119294ebcb30549f98a
Ciphercrypt <avimohite2019@gmail.com>
2023-01-31 14:45:19 +0530

    minor change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,8 +51,8 @@ export default function Home() {
   return (
     <>
          
-         <LoginScreen />
-{/* 
+         {/* <LoginScreen/> */}
+
          
  <div className="flex justify-center">
       
@@ -74,7 +74,7 @@ setseeallinvites={setseeallinvites}
           {seeallinvites  &&   <InvitesPage /> }
         </div>
       </div>
-    </div>  */}
+    </div>  
 
     </>
    
